151d9349d25949d9a34b11bcd0c1b2b3c08c2a66
Obsidiam <lukasbownik99@gmail.com>
2023-08-28 00:46:44 +0200

    Secure access to storage hub against malicious connections through custom prepared WS connections if someone would get to know ids of buckets which are private.

--- Areas/Core/Controllers/Hubs/FileOperationHub.cs
+++ Areas/Core/Controllers/Hubs/FileOperationHub.cs
@@ -2,13 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using PikaCore.Areas.Core.Models.File;
 using PikaCore.Areas.Core.Queries;
-using PikaCore.Areas.Identity.Attributes;
-using Serilog;
+using PikaCore.Infrastructure.Adapters;
+using PikaCore.Infrastructure.Services;
 
 namespace PikaCore.Areas.Core.Controllers.Hubs
 {
@@ -16,31 +15,46 @@ namespace PikaCore.Areas.Core.Controllers.Hubs
     {
         private readonly IConfiguration _configuration;
         private readonly IMediator _mediator;
+        private readonly IStorage _storage;
 
         public FileOperationHub(
             IConfiguration configuration,
-            IMediator mediator
+            IMediator mediator,
+            IStorage storage
         )
         {
             _configuration = configuration;
             _mediator = mediator;
+            _storage = storage;
         }
-
+        
         public async Task List(string search, string categoryId, string buckedId)
         {
-            if (string.IsNullOrEmpty(search))
+            var user = this.Context.GetHttpContext()?.User;
+            if (string.IsNullOrEmpty(search) || user == null)
             {
-                await this.Clients.User(this.Context.UserIdentifier).SendAsync("ReceiveListing", new
+                await this.Clients.Client(this.Context.ConnectionId).SendAsync("ReceiveListing", new
                 {
                     status = false,
                     listing = new List<ObjectInfo>()
                 });
+                return;
+            }
+
+            if (!await _storage.UserHasBucketAccess(Guid.Parse(buckedId), user))
+            {
+                await this.Clients.Client(this.Context.ConnectionId).SendAsync("ReceiveListing", new
+                {
+                    status = false,
+                    listing = new List<ObjectInfo>()
+                });
+                return; 
             }
 
             var listing = await _mediator.Send(
                 new FindAllObjectsByNameQuery(search, categoryId, buckedId)
             );
-            await this.Clients.User(this.Context.UserIdentifier).SendAsync("ReceiveListing", new
+            await this.Clients.Client(this.Context.ConnectionId).SendAsync("ReceiveListing", new
             {
                 status = true,
                 listing
